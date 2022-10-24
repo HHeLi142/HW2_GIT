ab5216d5202c4c1a9bbd3fe6d9d8f7a920c12623
Fedor <f.bokof.f@yandex.ru>
2022-10-24 05:34:28 +0300

    Added cnn in nn

--- Models/nn.py
+++ Models/nn.py
@@ -9,3 +9,13 @@ class nn():
 
     def predict(self):
         pass
+
+class cnn():
+    def __init__(self):
+        pass
+    
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
