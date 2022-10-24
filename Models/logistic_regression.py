3226f089758206b5a6141b1b55dd7ca3dc49427b
Fedor <f.bokof.f@yandex.ru>
2022-10-24 05:23:29 +0300

    Added fit and predict in logostic regression

--- Models/logistic_regression.py
+++ Models/logistic_regression.py
@@ -4,3 +4,9 @@ import pandas as pd
 class LogisticRegression():
     def __init__(self):
         pass
+
+    def fit(self, X_train, y_train):
+        pass
+
+    def predict(self, X_test):
+        pass
