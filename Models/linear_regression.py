8c9209a2a4e28293c964007e1d4880017f1c00d2
Fedor <f.bokof.f@yandex.ru>
2022-10-24 05:11:17 +0300

    Added linear regression

---
+++ Models/linear_regression.py
@@ -0,0 +1,10 @@
+from msilib.schema import Class
+from statistics import LinearRegression
+import numpy as np
+import pandas as pd
+
+
+class LinearRegression():
+    
+    def __init__(self):
+        pass
